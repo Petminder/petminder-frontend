f458bf4c1e3078ae1c47c6db7c68feb1c06b9f02
Jeff Meyer <jcmeyer10@gmail.com>
2015-12-13 23:29:58 -0500

    makes index reference new js files.  am able to register on the front end.

---
+++ js/clickhandlers.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+
+var form2object = function(form) {
+    var data = {};
+    $(form).find('input').each(function(index, element) {
+      var type = $(this).attr('type');
+      if ($(this).attr('name') && type !== 'submit' && type !=='hidden') {
+        data[$(this).attr('name')] = $(this).val();
+      }
+    });
+    return data;
+  };
+  var wrap = function wrap(root, formData) {
+    var wrapper = {};
+    wrapper[root] = formData;
+    return wrapper;
+  };
+
+  var callback = function callback(error, data) {
+    if (error) {
+      console.error(error);
+      $('#result').val('status: ' + error.status + ', error: ' + error.error);
+      return;
+    }
+    $('#result').val(JSON.stringify(data, null, 4));
+  };
+
+  $('#register').on('submit', function(e) {
+    e.preventDefault();
+    var credentials = wrap('credentials', form2object(this));
+    petminder_api.register(credentials, callback);
+    $('#register').hide();
+    console.log(data.user.token);
+  });
+
+  $('#login').on('submit', function(e) {
+    e.preventDefault();
+    var credentials = wrap('credentials', form2object(this));
+    var cb = function cb(error, data) {
+      if (error) {
+        callback(error);
+        return;
+      }
+      callback(null, data);
+      $('.token').val(data.user.token);
+      $('.id').val(data.user.id);
+      petminder_api.login(credentials, callback);
+      console.log(data.user.token);
+    };
+  });
+
+  $('.logout').on('click', function(e) {
+    var token = $('.token').val();
+    var id = $('.id').val();
+    var cb = function cb(error, data) {
+      if (error) {
+        callback(error);
+        return;
+      }
+    };
+    petminder_api.logout(token, id, cb);
+  });
+
+//end Document Ready
+});
