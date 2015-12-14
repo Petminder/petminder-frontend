d9cffb1614f8cd4fb5ef7f04bf83ea6b39d5b035
Jeff Meyer <jcmeyer10@gmail.com>
2015-12-13 21:59:27 -0500

    adds basic template for front end, changes header background image, the nav bar to add log in, register and changes some generic template filler for application specific language

---
+++ js/api.js
@@ -0,0 +1,46 @@
+var petminder_api = {
+  url: 'http://localhost:3000',
+
+  ajax: function (config, cb) {
+    $.ajax(config).done(function(data, textStatus, jqxhr) {
+      cb(null, data);
+    }).fail(function(jqxhr, status, error) {
+      cb({jqxher: jqxhr, status: status, error: error});
+    });
+  },
+
+  register: function (credentials, callback) {
+    this.ajax({
+      method: 'POST',
+      url: this.url + '/register',
+      contentType: 'application/json',
+      data: JSON.stringify(credentials),
+      dataType: 'json'
+    }, callback);
+    console.log("register success");
+  },
+
+  login: function (credentials, callback) {
+    this.ajax({
+      method: 'POST',
+      url: this.url + '/login',
+      contentType: 'application/json',
+      data: JSON.stringify(credentials),
+      dataType: 'json'
+    }, callback);
+    console.log("login success");
+  },
+
+  logout: function (token, id, callback) {
+    this.ajax({
+      method: 'DELETE',
+      url: this.url + '/logout/' + id,
+      headers: {
+        Authorization: 'Token token=' + token
+      },
+      contentType: 'application/json',
+      dataType: 'json'
+    }, callback);
+    console.log(" logout success");
+  }
+};
